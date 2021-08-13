99bc7a0cfea5a7a5e8c096163e6d43154a7b4873
vlausargs <39726092+vlausargs@users.noreply.github.com>
2021-08-13 12:28:25 +0700

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -30,27 +30,32 @@ const styles= StyleSheet.create({
 const Home = ()=>{
     const [ClockState, setClockState] = useState();
     const [DateState, setDateState] = useState();
+   
     React.useEffect(()=>{
+        let isMounted = true; 
         setInterval(()=>{
-            const date = new Date();
-            Date.prototype.toShortFormat = function() {
-
-                let monthNames =["Jan","Feb","Mar","Apr",
-                                  "May","Jun","Jul","Aug",
-                                  "Sep", "Oct","Nov","Dec"];
-                
-                let day = this.getDate();
-                
-                let monthIndex = this.getMonth();
-                let monthName = monthNames[monthIndex];
-                
-                let year = this.getFullYear();
-                
-                return `${day} ${monthName} ${year}`;  
-            }
-            setClockState(date.toLocaleTimeString());
-            setDateState(date.toShortFormat())
+            if(isMounted){
+                const date = new Date();
+                Date.prototype.toShortFormat = function() {
+    
+                    let monthNames =["Jan","Feb","Mar","Apr",
+                                      "May","Jun","Jul","Aug",
+                                      "Sep", "Oct","Nov","Dec"];
+                    
+                    let day = this.getDate();
+                    
+                    let monthIndex = this.getMonth();
+                    let monthName = monthNames[monthIndex];
+                    
+                    let year = this.getFullYear();
+                    
+                    return `${day} ${monthName} ${year}`;  
+                }
+                setClockState(date.toLocaleTimeString());
+                setDateState(date.toShortFormat())
+            } 
         })
+        return () => { isMounted = false }
     },[]);
    
     function renderHeader() {
@@ -70,9 +75,6 @@ const Home = ()=>{
         )
     }
     function renderTodayDateTime() {
-   
-  
-    
         return (
             <View style={{
                 
@@ -93,14 +95,72 @@ const Home = ()=>{
             </View>
         )
     }
-    
+    function renderCompanyInfo() {
+        return (
+            <View style={{ marginTop:30}}>
+                <Text style={{...FONTS.h3,textAlign:'center'}}>
+                    Name
+                </Text>
+                <Text style={{...FONTS.h3,textAlign:'center'}}>
+                    Division - Company
+                </Text>
+            </View>
+        )
+    }
+    function renderCurrAttandanceInfo() {
+        return (
+            <View style={{...styles.shadow,backgroundColor:COLORS.white,paddingVertical:SIZES.padding, margin:20,borderRadius:20}}>
+                <View style={{marginVertical:5}}>
+                    <Text style={{...FONTS.h2,textAlign:'center',fontWeight: 'bold'}}>
+                      Office Location
+                    </Text>    
+                 </View>
+                <View style={{marginVertical:5}}> 
+                    <Text style={{...FONTS.h2,textAlign:'center'}}>
+                        Distance to Office:
+                    </Text>
+                </View>
+                <View style={{marginVertical:5}}> 
+                    <Text style={{...FONTS.body,textAlign:'center'}}>
+                        10 M  (within range)
+                    </Text>
+                </View>
+                <View style={{marginVertical:5}}> 
+                    <Text style={{...FONTS.h2,textAlign:'center'}}>
+                        Office Hour:
+                    </Text>
+                </View>
+                <View style={{marginVertical:5}}> 
+                    <Text style={{...FONTS.body,textAlign:'center'}}>
+                        09:00 - 17:00
+                    </Text>
+                </View>
+                <View style={{marginVertical:5}}> 
+                    <TouchableOpacity 
+                    style={{
+                        
+                        ...styles.shadow,backgroundColor:'#71BC68',
+                        justifyContent:'center',
+                        marginHorizontal:SIZES.padding*5,
+                        paddingVertical:SIZES.padding/2,
+                        borderRadius:20
+                    }}>
+                        <Text style={{...FONTS.h2,fontWeight: 'bold' ,textAlign:'center'}}>
+                            Check In
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
     return (
       <SafeAreaView style={styles.container}>
           <View style={{...styles.shadow,backgroundColor:COLORS.white, paddingBottom:SIZES.padding}}>
             {renderHeader()}
             {renderTodayDateTime() }
-
           </View>
+          {renderCompanyInfo()}
+          {renderCurrAttandanceInfo()}
       </SafeAreaView>
     )
 }
