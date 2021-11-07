f2d9e8597af3c0bdc4a2202dee1724bdcceae553
vlausargs <39726092+vlausargs@users.noreply.github.com>
2021-11-07 17:59:01 +0700

    update

--- screens/Register.js
+++ screens/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { 
     View, 
     Text, 
@@ -8,12 +8,12 @@ import {
     StyleSheet ,
     StatusBar,
     Alert,
-    BackHandler
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import LinearGradient from 'react-native-linear-gradient';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
+import {Picker} from '@react-native-picker/picker';
 
 import { useTheme } from 'react-native-paper';
 // import { Input } from 'react-native-elements';
@@ -38,247 +38,130 @@ const Register = ({navigation}) => {
             username: '',
             password: '',
         });
-    const handleBackButton= ()=>{
-        // console.log(data.steps)
-        switch(data.steps) {
-            case 1:
-                // navigation.goBack(null);
-                return false;
-                break;
-            default:
-                prev_step();
-                return true;
-                break;
-        }
 
-    }
-    useEffect(() => {
-        BackHandler.addEventListener('hardwareBackPress', handleBackButton);
-        return () => {
-            BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
-          };
-    });
-
-    /*const textInputChange = (val) => {
-        if( val.trim().length >= 4 ) {
-            setData({
-                ...data,
-                username: val,
-                check_textInputChange: true,
-                isValidUser: true
-            });
-        } else {
-            setData({
-                ...data,
-                username: val,
-                check_textInputChange: false,
-                isValidUser: false
-            });
-        }
-    }
-
-    const handlePasswordChange = (val) => {
-        if( val.trim().length >= 8 ) {
-            setData({
-                ...data,
-                password: val,
-                isValidPassword: true
-            });
-        } else {
-            setData({
-                ...data,
-                password: val,
-                isValidPassword: false
-            });
-        }
-    }
-
-    const updateSecureTextEntry = () => {
-        setData({
-            ...data,
-            secureTextEntry: !data.secureTextEntry
-        });
-    }
-
-    const handleValidUser = (val) => {
-        if( val.trim().length >= 4 ) {
-            setData({
-                ...data,
-                isValidUser: true
-            });
-        } else {
-            setData({
-                ...data,
-                isValidUser: false
-            });
-        }
-    }
-
-    const loginHandle = (userName, password) => {
-
-        const foundUser = Users.filter( item => {
-            return userName == item.username && password == item.password;
-        } );
-
-        if ( data.username.length == 0 || data.password.length == 0 ) {
-            Alert.alert('Wrong Input!', 'Username or password field cannot be empty.', [
-                {text: 'Okay'}
-            ]);
-            return;
-        }
-
-        if ( foundUser.length == 0 ) {
-            Alert.alert('Invalid User!', 'Username or password is incorrect.', [
-                {text: 'Okay'}
-            ]);
-            return;
-        }
-        signIn(foundUser);
-    }*/
-    const next_step = ()=>{
-        const steps = data.steps
-        setData({
-            ...data,
-            steps:steps+1
-        })
-        console.log(user.name)
-    }
-    const prev_step = ()=>{
-        const steps = data.steps;
-        setData({
-            ...data,
-            steps:steps-1
-        })
-        // console.log("masuk2")
-    }
-
-    const step1button = (val) =>{
-        const steps = data.steps;
-        setData({
-            ...data,
-            steps:steps+1,
-            type:val
-        });
-        console.log(val)
-    }
     return (
         <View style={styles.container}>
-            <Wizard>
+            <Wizard initialValues={{
+                username: '',
+                email:'',
+                password:'',
+                type:''
+                }}>
                 <Wizard.Step >
-                    <View style={styles.container}>
-                        <TextInput
+                    {({onChangeValue,values})=>
+                        (
+                            <View style={{flex:1,alignItems:'stretch',justifyContent:'center',}}>
+                                <Text style={{
+                                        ...FONTS.body3,
+                                        // marginVertical:SIZES.padding,
+                                        marginHorizontal:SIZES.padding,
+                                        textAlign: 'center',
+                                        alignSelf: 'stretch',
+                                    }}
+                                >
+                                    Full Name
+                                </Text> 
+                                <TextInput
                                     placeholder= 'Name'
-                                    inputStyle={{ textAlign: 'center' }}
-                                    placeholderTextColor={COLORS.black}
-                                    inputContainerStyle={styles.inputContainer}
-                                    disableFullscreenUI={true}
-                                    autoCapitalize = 'none'   
-                        />      
-                    </View>
+                                    style={styles.inputContainer}
+                                    onChangeText={text=> onChangeValue('name',text)}  
+                                    value={values.name}
+                                />
+                                <Text style={{
+                                        ...FONTS.body3,
+                                        // marginVertical:SIZES.padding,
+                                        marginHorizontal:SIZES.padding,
+                                        textAlign: 'center',
+                                        alignSelf: 'stretch',
+                                    }}
+                                >
+                                    Email
+                                </Text> 
+                                <TextInput
+                                    placeholder= 'Email'
+                                    style={styles.inputContainer}
+                                    onChangeText={text=> onChangeValue('email',text)}  
+                                    value={values.email}
+                                />
+                                <Text style={{
+                                        ...FONTS.body3,
+                                        // marginVertical:SIZES.padding,
+                                        marginHorizontal:SIZES.padding,
+                                        textAlign: 'center',
+                                        alignSelf: 'stretch',
+                                    }}
+                                >
+                                    Password
+                                </Text> 
+                                <TextInput
+                                    placeholder= 'Password'
+                                    style={styles.inputContainer}
+                                    onChangeText={text=> onChangeValue('password',text)}  
+                                    value={values.password}
+                                />
+                                <Text style={{
+                                        ...FONTS.body3,
+                                        // marginVertical:SIZES.padding,
+                                        marginHorizontal:SIZES.padding,
+                                        textAlign: 'center',
+                                        alignSelf: 'stretch',
+                                    }}
+                                >
+                                    Register As
+                                </Text> 
+                                <Picker
+                                    selectedValue={values.type}
+                                    itemStyle={{
+                                        ...FONTS.body3,
+                                        marginVertical:SIZES.padding,
+                                        marginHorizontal:SIZES.padding,
+                                        textAlign: 'center',
+                                        alignSelf: 'stretch',
+                                        backgroundColor: 'lightgrey',
+                                        borderColor:COLORS.lightGray
+                                    }}
+                                    onValueChange={(itemValue, itemIndex) => onChangeValue('type',itemValue)}
+                                > 
+                                    <Picker.Item label="PLEASE CHOOSE ONE" color="blue" value="" style={styles.inputContainer}/>
+                                    <Picker.Item label="Owner" color="blue" value="owner" style={styles.inputContainer}/>
+                                    <Picker.Item label="Employee" color="blue" value="employee" style={styles.inputContainer} />
+                                </Picker>
+                            </View>
+                        )
+                    }
                 </Wizard.Step>
                 <Wizard.Step>
-                    <Text>HELLO WORLD</Text>
+                   
+                    {({onChangeValue,values})=>
+                        (
+                            <View style={{flex:1,alignItems:'stretch',justifyContent:'center',}}>
+                                <View style={styles.buttonRow}>
+                                    <TouchableOpacity onPress={()=>{onChangeValue('type','employee')}}
+                                        style={styles.buttons}>
+                                            <Text style={{...FONTS.h3,fontWeight: 'bold' ,textAlign:'center', color: COLORS.white}}>
+                                                Employee
+                                            </Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={()=>{onChangeValue('type','owner')}}
+                                        style={styles.buttons}>
+                                            <Text style={{...FONTS.h3,fontWeight: 'bold' ,textAlign:'center', color: COLORS.white}}>
+                                                Owner
+                                            </Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        )
+                    }
                 </Wizard.Step>
                 <Wizard.Step>
-                    <Text>HELLO WORLD</Text>
+                    {()=>(
+                        <Text>HELLO WORLD</Text>
+                    )}
                 </Wizard.Step>
             </Wizard>
         </View>
     );
-    // switch (data.steps) {
-    //     case 1:
-    //         return (
-    //             <View style={styles.container}>
-    //             <View style={styles.textInput}>
-    //                     <TextInput
-    //                         placeholder= 'Name'
-    //                         inputStyle={{ textAlign: 'center' }}
-    //                         placeholderTextColor={COLORS.black}
-    //                         inputContainerStyle={styles.inputContainer}
-    //                         disableFullscreenUI={true}
-    //                         autoCapitalize = 'none'
-    //                         onChangeText={(text)=>{setUser({
-    //                             ...user,
-    //                             name:text
-    //                         })}}       
-    //                     />  
-    //                     <TextInput
-    //                         placeholder= 'Email'
-    //                         inputStyle={{ textAlign: 'center' }}
-    //                         placeholderTextColor={COLORS.black}
-    //                         inputContainerStyle={styles.inputContainer}
-    //                         disableFullscreenUI={true}
-    //                         autoCapitalize = 'none'           
-    //                     />  
-         
-    //                     <TextInput
-    //                         placeholder="Password"
-    //                         inputStyle={{ textAlign: 'center' }}
-    //                         placeholderTextColor={COLORS.black}
-    //                         inputContainerStyle={styles.inputContainer}
-    //                         disableFullscreenUI={true}
-    //                         secureTextEntry={data.secureTextEntry ? true : false}
-    //                         autoCapitalize="none"
-    //                     />                   
-    //                 <View style={styles.button}>
-    //                     <TouchableOpacity style={styles.Login} onPress={next_step}>
-    //                         <Text style={styles.textSign}>Next</Text>
-    //                     </TouchableOpacity>
-    //                 </View>
-    //             </View>
-    //           </View>
-
-    //         );
-    //         break;
-    //     case 2:
-    //         return (
-    //             <View style={styles.container}>
-    //                 {/* <Text>step 1 register</Text> */}
-    //                 <View style={styles.buttonRow}>
-    //                     <TouchableOpacity onPress={()=>{step1button('employee')}}
-    //                         style={styles.buttons}>
-    //                             <Text style={{...FONTS.h3,fontWeight: 'bold' ,textAlign:'center', color: COLORS.white}}>
-    //                                 Employee
-    //                             </Text>
-    //                     </TouchableOpacity>
-    //                     <TouchableOpacity onPress={()=>{step1button('owner')}}
-    //                         style={styles.buttons}>
-    //                             <Text style={{...FONTS.h3,fontWeight: 'bold' ,textAlign:'center', color: COLORS.white}}>
-    //                                 Owner
-    //                             </Text>
-    //                     </TouchableOpacity>
-    //                 </View>
-    //             </View>
-    //         );
-    //         break;
-    //     case 3:
-    //         switch(data.type){
-    //             case 'employee':
-    //                 return (
-    //                     <View style={styles.container}>
-
-    //                     </View>
-
-    //                   );
-    //                   break;
-    //                 case 'owner':
-    //                     return (
-    //                         <View style={styles.container}>
-
-    //                         </View>
-    //                     );
-    //                     break;
-    //                 default:
-    //                     return (
-    //                         <View style={styles.container}>
-
-    //                         </View>
-    //                     );
-    //                     break;
-    //         }
-           
-    //         break;
-    // }
+    
     
 };
 
@@ -287,12 +170,13 @@ export default Register;
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'space-evenly',
-      backgroundColor:COLORS.white,
+    //   justifyContent: 'space-evenly',
+      backgroundColor:COLORS.white
     },
     textInput: {
         alignItems:'center',
         justifyContent: 'center'
+        // justifyContent: 'center'
     },
     button: {
         alignItems: 'center',
@@ -311,7 +195,11 @@ const styles = StyleSheet.create({
         color:COLORS.white
     },
     inputContainer:{
-        alignSelf: 'center',
+        ...FONTS.body3,
+        marginVertical:SIZES.padding,
+        marginHorizontal:SIZES.padding,
+        textAlign: 'center',
+        alignSelf: 'stretch',
         backgroundColor: COLORS.lightGray,
     },
     buttonRow:{
--- screens/components/Register/Wizard.js
+++ screens/components/Register/Wizard.js
@@ -1,16 +1,55 @@
 import React, {PureComponent} from 'react';
-import {View,Text,Button} from 'react-native';
+import {View,Text,Button,TouchableOpacity, BackHandler} from 'react-native';
+import { COLORS, FONTS, SIZES } from '../../../constants';
 
 class Step extends PureComponent {
+    handleBackButton= ()=>{
+        // console.log(data.steps)
+        switch(this.props.currentIndex) {
+            case 0:
+                // navigation.goBack(null);
+                return false;
+                break;
+            default:
+                this.props.prevStep();
+                return true;
+                break;
+        }
+
+    }
+    componentDidMount() {
+        this.backHandler = BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+    }
+    componentWillUnmount() {
+        this.backHandler.remove();
+    }
+
     state = {  }
     render() {
         return (
             <View style={{flex:1}}>
              
-                {this.props.children} 
+                {this.props.children({
+                    onChangeValue:this.props.onChangeValue,
+                    values:this.props.values,
+                })} 
 
-                <Button title="Next" disabled={this.props.isLast} onPress={this.props.nextStep}/>
-                <Button title="Prev" disabled={this.props.isFirst} onPress={this.props.prevStep}/>
+                <TouchableOpacity  
+                    title="Next" 
+                    disabled={this.props.isLast}
+                    onPress={this.props.nextStep} 
+                    style={{textAlign: 'center',alignSelf: 'stretch',backgroundColor: COLORS.primary, margin:SIZES.padding/2,paddingVertical:SIZES.padding/2,opacity:this.props.isLast ?  0.7:1}}
+                    >
+                    <Text style={{...FONTS.h3,textAlign: 'center'}}>Next</Text>
+                </TouchableOpacity >
+                <TouchableOpacity  
+                    title="Prev" 
+                    disabled={this.props.isFirst}
+                    onPress={this.props.prevStep} 
+                    style={{textAlign: 'center',alignSelf: 'stretch',backgroundColor: COLORS.primary, margin:SIZES.padding/2,paddingVertical:SIZES.padding/2,opacity:this.props.isFirst ? 0.7:1}}
+                    >
+                    <Text style={{...FONTS.h3,textAlign: 'center'}}>Prev</Text>
+                </TouchableOpacity >
             </View>
         );
     }
@@ -20,7 +59,10 @@ class Step extends PureComponent {
 class Wizard extends PureComponent {
     static Step = (props) => <Step {...props}/>
     state = { 
-        index: 0
+        index: 0,
+        values:{
+            ...this.props.initialValues,
+        }
     }
 
     _nextStep = () =>{
@@ -37,8 +79,18 @@ class Wizard extends PureComponent {
             }))
         }
     }
+    _onChangeValue = (name,value)=>{
+        this.setState(prevState =>({
+                values:{
+                    ...prevState.values,
+                    [name]:value,
+                }
+            })
+        )
+    }
 
     render() {
+        console.log('values', this.state)
         return (
             <View style={{flex:1}}>
                 {React.Children.map(this.props.children,(el,index)=>{
@@ -47,9 +99,10 @@ class Wizard extends PureComponent {
                             currentIndex: this.state.index,
                             nextStep:this._nextStep,
                             prevStep:this._prevStep,
+                            onChangeValue:this._onChangeValue,
                             isLast:this.state.index == this.props.children.length -1,
                             isFirst:this.state.index == 0,
-
+                            values:this.state.values,
                         })
                     }
                     return null;
