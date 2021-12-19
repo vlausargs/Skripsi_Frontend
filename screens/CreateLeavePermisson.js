7695b34ff8359f1d26edce06acb7cd28c7d6f55a
Vivaldy Augusta M.I <39726092+vlausargs@users.noreply.github.com>
2021-12-20 02:18:26 +0700

    create-permission

--- screens/CreateLeavePermisson.js
+++ screens/CreateLeavePermisson.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AsyncStorage from "@react-native-community/async-storage";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import {
     View,
     Text,
@@ -12,6 +13,8 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import { api_path, COLORS, FONTS, icons, SIZES } from "../constants";
 import { Alert } from "react-native";
 import { Image } from "react-native-elements";
+import { Picker } from "@react-native-picker/picker";
+
 
 
 const styles = StyleSheet.create({
@@ -28,12 +31,21 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 3,
         elevation: 10,
+    },
+    inputContainer: {
+        ...FONTS.h4,
     }
 })
 export default function CreateLeavePermisson({ navigation }) {
     const [currToken, setToken] = useState(null);
+    const [permissionRule, setPermissionRule] = useState([]);
     const [isInitData, setisInitData] = useState(true);
-
+    const [showDate, setShowDate] = useState({ start: false, end: false });
+    const [userInput, setUserInput] = useState({
+        'permission_type_id': null,
+        'start_date': new Date(),
+        'end_date': new Date(),
+    });
     async function _getTokenValue() {
         var value = await AsyncStorage.getItem('token')
         return value
@@ -47,15 +59,17 @@ export default function CreateLeavePermisson({ navigation }) {
                     "ERROR!!!",
                     "TOKEN EXPIRED",
                     [
-                        { text: "OK", onPress: () => navigation.reset({
-                            index: 1,
-                            routes: [
-                              {
-                                name: 'Settings',
-                                params: { someParam: 'Param1' },
-                              },
-                            ],
-                          }) }
+                        {
+                            text: "OK", onPress: () => navigation.reset({
+                                index: 1,
+                                routes: [
+                                    {
+                                        name: 'Settings',
+                                        params: { someParam: 'Param1' },
+                                    },
+                                ],
+                            })
+                        }
                     ]
                 );
 
@@ -65,11 +79,59 @@ export default function CreateLeavePermisson({ navigation }) {
         })
     }
 
+    function getPermissionRule() {
+        fetch(api_path + '/api/permission/getPermissionLeft', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + currToken,
+                'Accept': 'application/json'
+            }
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json)
+                setPermissionRule(json.result)
+
+            })
+            .catch((error) => console.error(error))
+            .finally(() => { });
+    }
+
+    function postPermissionRule() {
+        fetch(api_path + '/api/permission/create', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + currToken,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify({
+                "permission_type_id":userInput.permission_type_id,
+                "start_date":userInput.start_date,
+                "end_date":userInput.end_date
+            })
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json)
+
+            })
+            .catch((error) => console.error(error))
+            .finally(() => { });
+    }
+
     React.useEffect(() => {
         if (isInitData == true) {
             checkToken();
         }
     }, [isInitData]);
+
+    React.useEffect(() => {
+        if (isInitData == true && currToken) {
+            getPermissionRule();
+        }
+    }, [isInitData, currToken]);
+
     function renderHeader() {
         return (
             <View style={{ ...styles.shadow, backgroundColor: COLORS.white, paddingBottom: SIZES.padding }}>
@@ -81,22 +143,22 @@ export default function CreateLeavePermisson({ navigation }) {
                             paddingLeft: SIZES.padding * 2,
                             paddingTop: SIZES.padding,
                             justifyContent: 'center'
-                            
+
                         }}
-                        onPress={()=>{
+                        onPress={() => {
                             navigation.goBack();
                         }}
-                        >
+                    >
                         <Image
                             source={icons.back}
                             resizeMode="contain"
                             style={{
                                 width: 25,
                                 height: 25,
-                                tintColor:  COLORS.primary 
+                                tintColor: COLORS.primary
                             }}
                         />
-                        
+
                     </TouchableOpacity>
                     <View style={{
 
@@ -112,12 +174,118 @@ export default function CreateLeavePermisson({ navigation }) {
 
         )
     }
+    function changeType(value) {
+        console.log(userInput)
+        setUserInput({ ...userInput, 'permission_type_id': value })
+    }
+    function renderForm() {
+        return (
+            <View style={{ ...styles.shadow, backgroundColor: COLORS.white, paddingBottom: SIZES.padding, marginVertical: SIZES.padding, flex: 1 }}>
+                <View style={{ marginTop: SIZES.padding, marginHorizontal: SIZES.padding }}>
+                    <Text style={{ ...FONTS.h3, fontWeight: 'bold' }}>
+                        Types
+                    </Text>
+                    <View>
+                        <Picker
+                            selectedValue={userInput.permission_type_id}
+                            style={{
+                                ...styles.shadow,
+                                marginVertical: SIZES.padding,
+                                marginHorizontal: SIZES.padding,
+                                textAlign: 'center',
+                                alignSelf: 'stretch',
+                                backgroundColor: 'white',
+                                borderColor: COLORS.lightGray
+                            }}
+                            // itemStyle={{...FONTS.h1,}}
+                            onValueChange={(itemValue, itemIndex) => changeType(itemValue)}
+                        >
+                            <Picker.Item label="PLEASE CHOOSE ONE" color="black" value="" style={styles.inputContainer} />
+                            {permissionRule.map((item, key) => {
+                                return (
+                                    <Picker.Item label={item.leave_type + " (" + item.left_permission + " days)"} color="black" value={item.id} style={styles.inputContainer} key={key} />
+                                )
+                            })}
+
+                            {/* <Picker.Item label="Employee" color="blue" value="employee" style={styles.inputContainer} /> */}
+                        </Picker>
+                        {/* dropdown leave type */}
+                    </View>
+                    <Text style={{ ...FONTS.h3, fontWeight: 'bold' }}>
+                        Start Date
+                    </Text>
+                    <View>
+                        <TouchableOpacity
+                            style={{
+                                ...styles.shadow,
+                                 backgroundColor: COLORS.white,
+                                paddingVertical:SIZES.padding*1.5,
+                                marginVertical: SIZES.padding,
+                                marginHorizontal: SIZES.padding
+                            }}
+                            onPress={() => {
+                                setShowDate({...showDate,start:true})
+                            }}
+                        >
+                           <Text style={styles.inputContainer}>{userInput.start_date.toLocaleDateString()}</Text>
+
+                        </TouchableOpacity>
+                        {showDate.start &&(<DateTimePicker
+                            testID="startDate"
+                            value={userInput.start_date}
+                            mode="date"
+                            display="default"
+                            on
+                            onChange={(e,selectedValue)=>{
+                                setUserInput({...userInput,start_date:selectedValue||userInput.start_date})
+                                setShowDate({...showDate,start:false})
+                            }}
+                        />)}
+                        {/* dropdown leave type */}
+                    </View>
+                    <Text style={{ ...FONTS.h3, fontWeight: 'bold' }}>
+                        End Date
+                    </Text>
+                    <View>
+                    <TouchableOpacity
+                            style={{
+                                ...styles.shadow,
+                                 backgroundColor: COLORS.white,
+                                paddingVertical:SIZES.padding*1.5,
+                                marginVertical: SIZES.padding,
+                                marginHorizontal: SIZES.padding
+                            }}
+                            onPress={() => {
+                                setShowDate({...showDate,end:true})
+                            }}
+                        >
+                           <Text style={styles.inputContainer}>{userInput.end_date.toLocaleDateString()}</Text>
+
+                        </TouchableOpacity>
+                      
+                        {showDate.end &&(<DateTimePicker
+                            testID="endDate"
+                            value={userInput.end_date}
+                            mode="date"
+                            display="default"
+                            onChange={(e,selectedValue)=>{
+                                setUserInput({...userInput,end_date:selectedValue||userInput.end_date})
+                                setShowDate({...showDate,end:false})
+                            }}
+                        />)}
+                        {/* dropdown leave type */}
+                    </View>
+                </View>
+            </View>
+        )
+    }
     return (
-        <View>
-            {renderHeader()}
-            <Text>
-                create leave permission
-            </Text>
-        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                {renderHeader()}
+                {renderForm()}
+
+            </View>
+        </SafeAreaView>
     )
 }
